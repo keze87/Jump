6376e5c98d40c64b805452140f733383b809f5a4
keze <keze87@gmail.com>
2018-06-01 20:44:09 -0300

    Init?

---
+++ src/CT04.c
@@ -0,0 +1,24 @@
+// César Ezequiel Herrera 97429
+// Numero de grupo: 4
+//
+// Make:
+// gcc -o CT04 CT04.c -lm -std=c11 -Wall -pedantic -pedantic-errors -I.
+
+#include <stdio.h>
+
+#define TRUE 0
+#define FALSE 1
+
+int proceso () {
+
+	return TRUE;
+
+}
+
+int main () {
+
+	printf ("\nCesar Ezequiel Herrera 97429\n");
+
+	return proceso ();
+
+}
